f3e7df9989b206ed0464a2822c257386fd596661
Jose Añasco <joseanasco1@gmail.com>
2015-04-02 18:48:59 -0500

    fix some testing issues for sources

--- test/mixtape/source/nme_test.rb
+++ test/mixtape/source/nme_test.rb
@@ -1,83 +1,43 @@
 require 'test_helper'
 
-describe Mixtape::Source::Nme, :vcr do
+describe Mixtape::Source::Nme, vcr: { record: :once } do
   describe ".best_new_tracks" do
-    it "picks 5 songs from the first page, 3 from the second and 2 for the last" do
-      page_one_songs = [
-        Mixtape::Song.new("The Vaccines", "Dream Lover"),
-        Mixtape::Song.new("Brandon Flowers", "Can’t Deny My Love"),
-        Mixtape::Song.new("Kendrick Lamar", "King Kunta"),
-        Mixtape::Song.new("Lusts", "Mouthwash"),
-        Mixtape::Song.new("Bully", "I Remember"),
-        Mixtape::Song.new("Hudson Mohawke", "Very First Breath"),
-        Mixtape::Song.new("Grimes x Bleachers", "Entropy"),
-        Mixtape::Song.new("Pill", "Misty Eyed Porno Reader"),
-        Mixtape::Song.new("Future Flocka", "Rotation"),
-        Mixtape::Song.new("Jonny Greenwood", "Lola Choir"),
-        Mixtape::Song.new("Sufjan Stevens", "Carrie & Lowell"),
-        Mixtape::Song.new("Death Grips", "On GP"),
-        Mixtape::Song.new("Sheer Mag", "Button Up"),
-        Mixtape::Song.new("Jaakko Eino Kalevi", "Double Talk"),
-        Mixtape::Song.new("Years & Years", "Worship"),
-        Mixtape::Song.new("James Murphy", "We Used To Dance"),
-        Mixtape::Song.new("Blur", "There Are Too Many Of Us"),
-        Mixtape::Song.new("Nai Harvest", "All The Time"),
-        Mixtape::Song.new("Rolo Tomassi", "Stage Knives"),
-        Mixtape::Song.new("Girls Names", "Zero Triptych")
-      ]
-      page_three_songs = [
-        Mixtape::Song.new("M83 & Haim", "Holes In The Sky"),
-        Mixtape::Song.new("Mumford & Sons", "Believe"),
-        Mixtape::Song.new("Crows", "Crawling"),
-        Mixtape::Song.new("Drenge", "Never Awake"),
-        Mixtape::Song.new("Major Lazer", "Lean On"),
-        Mixtape::Song.new("Gengahr", "Haunter"),
-        Mixtape::Song.new("Kelela", "A Message"),
-        Mixtape::Song.new("Faith No More", "Superhero"),
-        Mixtape::Song.new("Doldrums", "Loops"),
-        Mixtape::Song.new("Holly Miranda", "All I Want Is To Be Your Girl"),
-        Mixtape::Song.new("Action Bronson", "Baby Blue (feat. Chance The Rapper)"),
-        Mixtape::Song.new("Pins", "Too Little Too Late"),
-        Mixtape::Song.new("Outfit", "Genderless"),
-        Mixtape::Song.new("Vant", "Parasite"),
-        Mixtape::Song.new("Toro Y Moi", "Buffalo"),
-        Mixtape::Song.new("My Morning Jacket", "Big Decisions"),
-        Mixtape::Song.new("Speedy Ortiz", "The Graduates"),
-        Mixtape::Song.new("Braids", "Taste"),
-        Mixtape::Song.new("Kanye West", "U Mad feat. Vic Mensa"),
-        Mixtape::Song.new("Holly Herndon", "Interference")
-      ]
-      page_five_songs = [
-        Mixtape::Song.new("Blur", "Go Out"),
-        Mixtape::Song.new("Wolf Alice", "Giant Peach"),
-        Mixtape::Song.new("Chance The Rapper & The Social Experiment", "Lady Friend"),
-        Mixtape::Song.new("Sufjan Stevens", "No Shade In The Shadow Of The Cross"),
-        Mixtape::Song.new("Action Bronson", "Terry"),
-        Mixtape::Song.new("Jon Hopkins", "I Remember"),
-        Mixtape::Song.new("Modest Mouse", "The Ground Walks, With Time In A Box"),
-        Mixtape::Song.new("East India Youth", "Turn Away"),
-        Mixtape::Song.new("Angel Haze", "Gxmes"),
-        Mixtape::Song.new("Father John Misty", "Heart-Shaped Box"),
-        Mixtape::Song.new("Earl Sweatshirt", "Quest/Power"),
-        Mixtape::Song.new("Big Sean", "One Man Can Change The World"),
-        Mixtape::Song.new("Together Pangea", "I Looked In Too"),
-        Mixtape::Song.new("Pussy Riot", "I Can't Breathe (feat. Richard Hell)"),
-        Mixtape::Song.new("Jacco Gardner", "Find Yourself"),
-        Mixtape::Song.new("Torres", "Sprinter"),
-        Mixtape::Song.new("Chromatics", "Just Like You"),
-        Mixtape::Song.new("White Shadows", "Give Up Give Out Give In"),
-        Mixtape::Song.new("Questlove/ Black Thought/ Charli XCX", "Groove Is In The Heart"),
-        Mixtape::Song.new("Twin Shadow", "I’m Ready")
-      ]
-      top_ten_tracks = Mixtape::Source::Nme.best_new_tracks
+    let(:first_page_songs) do [
+      Mixtape::Song.new("The Maccabees", "'Marks To Prove It'"),
+      Mixtape::Song.new("Florence + The Machine", "St Jude"),
+      Mixtape::Song.new("Brandon Flowers", "Dreams Come True"),
+      Mixtape::Song.new("Muse ", "Dead Inside"),
+      Mixtape::Song.new("Brian Wilson", "Sail Away"),
+      Mixtape::Song.new("Dutch Uncles", "Realm"),
+      Mixtape::Song.new("Shamir", "Call It Off"),
+      Mixtape::Song.new("Wale, Kanye West & Ty Dolla $ign", "The Summer League"),
+      Mixtape::Song.new("Communions", "Out Of My World"),
+      Mixtape::Song.new("Du Blonde", "Mind Is On My Mind"),
+      Mixtape::Song.new("Antwon", "Jacuzzi (feat. Lushlife)"),
+      Mixtape::Song.new("Earl Sweatshirt & Action Bronson", "Warlord Leather"),
+      Mixtape::Song.new("Wolf Alice", "I Saw You (In A Corridor)"),
+      Mixtape::Song.new("Jamie xx", "Loud Places (feat. Romy)"),
+      Mixtape::Song.new("Donald Cumming", "Game Of The Heart"),
+      Mixtape::Song.new("Leftfield", "Universal Everything"),
+      Mixtape::Song.new("FKA Twigs", "Glass & Patron"),
+      Mixtape::Song.new("Summer Camp", "Bad Love"),
+      Mixtape::Song.new("Ceremony", "The Separation"),
+      Mixtape::Song.new("Blur", "Lonesome Street"),
+    ] end
 
-      intersection_page_one   = top_ten_tracks[0..4] & page_one_songs
-      intersection_page_three = top_ten_tracks[5..7] & page_three_songs
-      intersection_page_five  = top_ten_tracks[8..9] & page_five_songs
+    let(:top_ten_tracks) { Mixtape::Source::Nme.best_new_tracks }
 
-      intersection_page_one.wont_be_empty
-      intersection_page_three.wont_be_empty
-      intersection_page_five.wont_be_empty
+    it "returns 10 songs" do
+      top_ten_tracks.length.must_equal 10
+    end
+
+    it "takes songs them from nme's best new tracks" do
+      assert first_page_songs.any? { |song| top_ten_tracks.include? song }
+    end
+
+    it 'picks them randomly' do
+      more_top_ten_tracks = Mixtape::Source::Nme.best_new_tracks
+      more_top_ten_tracks.wont_equal top_ten_tracks
     end
   end
 end
--- test/mixtape/source/pitchfork_test.rb
+++ test/mixtape/source/pitchfork_test.rb
@@ -1,66 +1,28 @@
 require 'test_helper'
 
-describe Mixtape::Source::Pitchfork, :vcr do
+describe Mixtape::Source::Pitchfork, vcr: { record: :once } do
   describe ".best_new_tracks" do
-    let(:songs) do [
-        Mixtape::Song.new("iLoveMakonnen", "Whip It Remix ft Migos and Rich the Kid"),
-        Mixtape::Song.new("Cakes Da Killa", "Serve It Up"),
-        Mixtape::Song.new("Jamie xx","Gosh"),
-        Mixtape::Song.new("Prurient","Dragonflies to Sew You Up"),
-        Mixtape::Song.new("Jamie xx","Loud Places ft Romy"),
-        Mixtape::Song.new("Galcher Lustwerk","Parlay"),
-        Mixtape::Song.new("Ziq","XT"),
-        Mixtape::Song.new("Earl Sweatshirt","Grief"),
-        Mixtape::Song.new("Kendrick Lamar","King Kunta"),
-        Mixtape::Song.new("Death Grips","On GP"),
-        Mixtape::Song.new("Lower Dens","Ondine"),
-        Mixtape::Song.new("Sheer Mag","Button Up"),
-        Mixtape::Song.new("Sufjan Stevens", "Should Have Known Better"),
-        Mixtape::Song.new("Tame Impala", "Let It Happen"),
-        Mixtape::Song.new("Grimes", "REALiTi"),
-        Mixtape::Song.new("Soko", "Lovetrap ft Ariel Pink"),
-        Mixtape::Song.new("Chromatics", "I Can Never Be Myself When Youre Around"),
-        Mixtape::Song.new("Jenny Hval", "That Battle Is Over"),
-        Mixtape::Song.new("Westkust", "Swirl"),
-        Mixtape::Song.new("Tink", "Ratchet Commandments"),
-        Mixtape::Song.new("Courtney Barnett", "Depreston"),
-        Mixtape::Song.new("Sannhet", "Revisionist"),
-        Mixtape::Song.new("Sufjan Stevens", "No Shade in the Shadow of the Cross"),
-        Mixtape::Song.new("Drake", "Energy"),
-        Mixtape::Song.new("Hot Chip", "Huarache Lights"),
-        Mixtape::Song.new("Speedy Ortiz", "Raising the Skate"),
-        Mixtape::Song.new("Kendrick Lamar", "The Blacker the Berry"),
-        Mixtape::Song.new("Unknown Mortal Orchestra", "MultiLove"),
-        Mixtape::Song.new("Makthaverskan", "Witness"),
-        Mixtape::Song.new("Courtney Barnett", "Pedestrian at Best"),
-        Mixtape::Song.new("Lower Dens", "To Die in LA"),
-        Mixtape::Song.new("Jazmine Sullivan", "Dumb ft Meek Mill"),
-        Mixtape::Song.new("Frank Ocean", "At Your Best You Are Love Aaliyah/The Isley Brothers Cover"),
-        Mixtape::Song.new("Charli XCX", "Doing It ft Rita Ora"),
-        Mixtape::Song.new("Aphex Twin", "Diskhat ALL Prepared1mixed snr2mix"),
-        Mixtape::Song.new("Tobias Jesso Jr", "How Could You Babe"),
-        Mixtape::Song.new("Arca", "Sheep"),
-        Mixtape::Song.new("Natalie Prass", "My Baby Dont Understand Me"),
-        Mixtape::Song.new("Waxahatchee", "Air"),
-        Mixtape::Song.new("Twerps", "I Dont Mind"),
+    let(:first_page_songs) do [
+      Mixtape::Song.new("iLoveMakonnen", "Whip It Remix ft Migos and Rich the Kid"),
+      Mixtape::Song.new("Cakes Da Killa", "Serve It Up"),
+      Mixtape::Song.new("Jamie xx","Gosh"),
+      Mixtape::Song.new("Prurient","Dragonflies to Sew You Up"),
+      Mixtape::Song.new("Jamie xx","Loud Places ft Romy"),
     ] end
 
-    context "number of songs" do
-      let(:top_ten_tracks) { Mixtape::Source::Pitchfork.best_new_tracks }
+    let(:top_ten_tracks) { Mixtape::Source::Pitchfork.best_new_tracks }
 
-      it "returns 10 songs" do
-        top_ten_tracks.length.must_equal 10
-      end
-
-      it "takes them from pitchfork's best new tracks" do
-        intersection = top_ten_tracks & songs
-        intersection.length.must_equal 10
-      end
+    it "returns 10 songs" do
+      top_ten_tracks.length.must_equal 10
     end
 
-    it 'picks them randomly with more recent ones' do
-      Mixtape::RandomSongs.any_instance.expects(:most_recent_pick).once
-      Mixtape::Source::Pitchfork.best_new_tracks
+    it "takes them from pitchfork's best new tracks" do
+      assert first_page_songs.any? { |song| top_ten_tracks.include? song }
+    end
+
+    it 'picks them randomly' do
+      more_top_ten_tracks = Mixtape::Source::Pitchfork.best_new_tracks
+      more_top_ten_tracks.wont_equal top_ten_tracks
     end
   end
 end

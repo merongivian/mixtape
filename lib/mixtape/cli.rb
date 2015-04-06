6914af8ed8a621535a8fd9ab7334a7aa1f72732c
Jose Añasco <joseanasco1@gmail.com>
2015-04-06 16:23:57 -0500

    awesome output :sparkling_heart:

--- lib/mixtape/cli.rb
+++ lib/mixtape/cli.rb
@@ -7,25 +7,43 @@ module Mixtape
     def initialize(source_name, number_of_songs)
       source_constant = Mixtape::Source.const_get(source_name.capitalize)
       @source = source_constant.new(number_of_songs)
+      @source_name = source_name
       rescue NameError; raise InvalidSource
     end
 
     def open_playlist
-      system "echo '   ____________________________'"
-      system "echo ' /|............................|'"
-      system "echo '| |:      AwesomeMixtape      :|'"
-      system "echo '| |:                          :|'"
-      system "echo '| |:     ,-.   _____   ,-.    :|'"
-      system "echo '| |:    ( `)) [_____] ( `))   :|'"
-      system "echo '|v|:     `-`   , , ,   `-`    :|'"
-      system "echo '|||:     ,______________.     :|'"
-      system "echo '|||...../::::o::::::o:::::.....|'"
-      system "echo '|^|..../:::O::::::::::O::::....|'"
-      system "echo '|/`---/--------------------`---|'"
-      system "echo '`.___/ /====/ /=//=/ /====/,___/'"
-      system "echo '     `--------------------.'"
+      system "echo 'Creating mixtape...'"
       playlist =  Mixtape::YoutubePlaylist.new(@source.songs)
-      Launchy.open(playlist.url)
+      launch = Thread.new { Launchy.open(playlist.url) }
+      mixtape_animation = Thread.new { awesome_output }
+      mixtape_animation.join
+      launch.join
+    end
+
+    private
+
+    def awesome_output
+      num_of_spaces = (26 - @source_name.length)/2
+      spaces = " " * num_of_spaces
+      title = spaces + @source_name + spaces
+      mixtape_title = "| |:#{title} :|"
+      tape = <<-TAPE
+         ____________________________
+       /|............................|
+      | |:      AwesomeMixtape      :|
+      #{mixtape_title}
+      | |:     ,-.   _____   ,-.    :|
+      | |:    ( `)) [_____] ( `))   :|
+      |v|:     `-`   , , ,   `-`    :|
+      |||:     ,______________.     :|
+      |||...../::::o::::::o:::::.....|
+      |^|..../:::O::::::::::O::::....|
+      |/`---/--------------------`---|
+      `.___/ /====/ /=//=/ /====/,___/
+           `--------------------.
+      TAPE
+
+      system "echo '#{tape}' | lolcat --animate"
     end
   end
 end
--- mixtape.gemspec
+++ mixtape.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "nokogiri", "~> 1.6.6.2"
   spec.add_dependency "launchy", "~> 2.4.3"
   spec.add_dependency "pmap", "~> 1.0.2"
+  spec.add_dependency "lolcat", "~> 42.1.0"
   spec.add_development_dependency "minitest-vcr", "~> 1.2.2"
   spec.add_development_dependency "webmock", "~> 1.20.4"
   spec.add_development_dependency "bundler", "~> 1.7"

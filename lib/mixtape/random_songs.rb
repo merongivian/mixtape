1947c9ca456790eab6db61c7caa5863f1468d786
Jose Añasco <joseanasco1@gmail.com>
2015-04-06 13:39:33 -0500

    clean up

--- lib/mixtape/random_songs.rb
+++ lib/mixtape/random_songs.rb
@@ -5,14 +5,8 @@ module Mixtape
     end
 
     def pick_by(number_per_section)
-      random_pick(@sliced_songs, number_per_section)
-    end
-
-    private
-
-    def random_pick(sliced_array, number_per_subarray)
-      sliced_array.map do |slice|
-        slice.sample(number_per_subarray)
+      @sliced_songs.map do |slice|
+        slice.sample(number_per_section)
       end.flatten
     end
   end

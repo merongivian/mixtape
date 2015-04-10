28953ed77fd6ce7bef7d406fab95534dfe090333
Jose Añasco <joseanasco1@gmail.com>
2015-04-10 10:27:19 -0500

    use flatmap

--- lib/mixtape/random_songs.rb
+++ lib/mixtape/random_songs.rb
@@ -5,9 +5,9 @@ module Mixtape
     end
 
     def pick_by(number_per_section)
-      @sliced_songs.map do |slice|
+      @sliced_songs.flat_map do |slice|
         slice.sample(number_per_section)
-      end.flatten
+      end
     end
   end
 end
